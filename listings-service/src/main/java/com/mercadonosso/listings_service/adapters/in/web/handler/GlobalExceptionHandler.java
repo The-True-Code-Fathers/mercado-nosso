2e5898bcc044b73db1cacc50e8af439efa04ec91
Zambrinn <zambrinfilho@gmail.com>
2025-07-02 10:19:05 -0300

    refactor: change some classes name

---
+++ listings-service/src/main/java/com/mercadonosso/listings_service/adapters/in/web/handler/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.mercadonosso.listings_service.adapters.in.web.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+}
