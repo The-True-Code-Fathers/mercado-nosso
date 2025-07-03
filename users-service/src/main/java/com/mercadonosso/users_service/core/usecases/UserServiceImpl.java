e30f35a375c7ace0f6479f1a3ea74e209cf065a6
Matheus de Bortoli Pastega <matheuspastega@gmail.com>
2025-07-03 12:56:03 -0300

    fix(user-service): Annotate UserServiceImpl with @Service for bean injection

--- users-service/src/main/java/com/mercadonosso/users_service/core/usecases/UserServiceImpl.java
+++ users-service/src/main/java/com/mercadonosso/users_service/core/usecases/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.mercadonosso.users_service.core.usecases;
 
 import com.mercadonosso.users_service.core.ports.in.UserServicePort;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserServicePort {
 }
