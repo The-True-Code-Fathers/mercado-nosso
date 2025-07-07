079ef477ffe88c22dff5ad5bb2e242f7667eef80
Matheus de Bortoli Pastega <matheuspastega@gmail.com>
2025-07-06 21:06:54 -0300

    refactor: remove unused UserServiceImpl class
    - Clean up users-service by removing empty implementation
    - UserService now properly implements UserServicePort interface
    - Maintains clean architecture without duplicate classes

--- users-service/src/main/java/com/mercadonosso/users_service/core/usecases/UserServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.mercadonosso.users_service.core.usecases;
-
-import com.mercadonosso.users_service.core.ports.in.UserServicePort;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserServiceImpl implements UserServicePort {
-}
