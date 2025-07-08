03e4aa7291508aa52e75b16cfd5f62d0961a3bd7
Nelson Antunes Junior <nelsoantunes2@gmail.com>
2025-07-08 08:41:10 -0300

    Orders frontend

--- orders-service/src/main/java/com/mercadonosso/orders_service/orderservice/adapters/in/OrderController.java
+++ orders-service/src/main/java/com/mercadonosso/orders_service/orderservice/adapters/in/OrderController.java
@@ -16,6 +16,7 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class OrderController {
     private final OrdersServicePort ordersServicePort;
 
