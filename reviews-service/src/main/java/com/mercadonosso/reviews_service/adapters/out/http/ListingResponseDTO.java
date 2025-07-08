7b86b8ca96dc43c16690de0db58f7fbfe1fe0a35
Zambrinn <zambrinfilho@gmail.com>
2025-07-08 15:38:28 -0300

    feat: integrate ListingsServiceAdapter for listing retrieval and update dependency management

--- reviews-service/src/main/java/com/mercadonosso/reviews_service/adapters/out/http/ListingResponseDTO.java
+++ reviews-service/src/main/java/com/mercadonosso/reviews_service/adapters/out/http/ListingResponseDTO.java
@@ -2,7 +2,7 @@ package com.mercadonosso.reviews_service.adapters.out.http;
 
 import java.util.UUID;
 
-public record ListingReponseDTO(
+public record ListingResponseDTO(
         UUID listingId,
         UUID sellerId
 ) {
